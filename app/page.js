58f42a633754a05ad18cea387262aa590c2d7cd7
shahriarlimon <96817971+shahriarlimon@users.noreply.github.com>
2023-06-04 14:22:59 +0600

    added basic carousel

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import CarouselSlide from '@/components/Navbar/Carousel/Carousel'
 import Navbar from '@/components/Navbar/Navbar'
 import Image from 'next/image'
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <main className="">
       <Navbar />
+      <CarouselSlide />
 
     </main>
   )
